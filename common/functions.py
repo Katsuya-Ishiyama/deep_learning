b97316ffa603cab583d5365f9012332377efffd3
Katsuya Ishiyama <ishiyama.katsuya@gmail.com>
2017-03-17 20:48:10 +0900

    Revert "Bug fix: index using at array "y""
    This reverts commit f5a60cf8e7b4bd128f4dff0d1426a989c9cc6c84.

--- common/functions.py
+++ common/functions.py
@@ -11,12 +11,12 @@ def step_function(x):
 
 
 def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    return 1 / (1 + np.exp(-x))    
 
 
 def sigmoid_grad(x):
     return (1.0 - sigmoid(x)) * sigmoid(x)
-
+    
 
 def relu(x):
     return np.maximum(0, x)
@@ -26,7 +26,7 @@ def relu_grad(x):
     grad = np.zeros(x)
     grad[x>=0] = 1
     return grad
-
+    
 
 def softmax(x):
     if x.ndim == 2:
@@ -47,13 +47,13 @@ def cross_entropy_error(y, t):
     if y.ndim == 1:
         t = t.reshape(1, t.size)
         y = y.reshape(1, y.size)
-
+        
     # 教師データがone-hot-vectorの場合、正解ラベルのインデックスに変換
     if t.size == y.size:
         t = t.argmax(axis=1)
-
+             
     batch_size = y.shape[0]
-    return -np.sum(np.log(y[range(batch_size), t.tolist()])) / batch_size
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
 
 
 def softmax_loss(X, t):
